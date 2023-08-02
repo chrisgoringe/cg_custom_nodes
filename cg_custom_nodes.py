fcca75edc01929df69a5a2c24669bd58e566387f
Chris <chris.goringe@gmail.com>
2023-08-02 12:03:45 +1000

    x

--- cg_custom_nodes.py
+++ cg_custom_nodes.py
@@ -43,7 +43,7 @@ class Concat:
     def concat(self, s1, s2, sep):
         return (f"{s1}{sep}{s2}",)
     
-class ToString:
+class XToString:
     def __init__(self):
         pass
 
@@ -51,32 +51,19 @@ class ToString:
     RETURN_NAMES = ("string",)
     FUNCTION = "x2s"
     CATEGORY = "CG"
-    PP = { "prefix": ("STRING", {"default":""}), "postfix": ("STRING", {"default":""}), }
 
-    
-class XToString(ToString):
     @classmethod    
     def INPUT_TYPES(s):
-        return { "required": s.PP,
+        return { "required": { "prefix": ("STRING", {"default":""}), "postfix": ("STRING", {"default":""}), },
                  "optional": { 
                     "int": ("INT", {}), 
                     "float": ("FLOAT", {}),
                     "string": ("STRING", {})
                  }}
+    
     def x2s(self, prefix, postfix, int, float, string):
         return (f"{prefix}{int or ''}{float or ''}{string or ''}{postfix}",)  
 
-class IntToString(ToString):
-    @classmethod    
-    def INPUT_TYPES(s):
-        return { "required": { "input": ("INT", {"default": 0}), } | s.PP }
-    def x2s(self, input, prefix, postfix):
-        return (f"{prefix}{input}{postfix}",)   
-class FloatToString(ToString):
-    @classmethod    
-    def INPUT_TYPES(s):
-        return { "required": { "input": ("FLOAT", {"default": 0.0}), } | s.PP }
-    
 class RandomFloats:
     def __init__(self):
         pass
