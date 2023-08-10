a9f89dc0eba2eae87403092b2985271ae7c6cf86
Chris <chris.goringe@gmail.com>
2023-08-11 09:12:16 +1000

    log

--- src/dev.py
+++ src/dev.py
@@ -1,9 +1,10 @@
 from src.base import Base
 
 def func(*args, **kwargs):
-    input = list(kwargs.values())[0]
-    print(f"{input}")                 # This is a good place to put a breakpoint...
-    return (input,)
+    input, log = list(kwargs.values())
+    if log:
+        print(f"{input}")                 
+    return (input,) # This is a good place to put a breakpoint...
 
 def inspect_factory(name:str, to_inspect:str, display_name:str=None):
     display_name = display_name or to_inspect
