05cddd35d2e2c96eebf7e7c27232b579482122bb
Chris <chris.goringe@gmail.com>
2023-09-06 10:43:05 +1000

    remove compare images

--- src/images.py
+++ src/images.py
@@ -7,19 +7,6 @@ from common_custom import module_resource_directory
 
 def mask_to_image(mask:torch.tensor):
     return mask.reshape((-1, 1, mask.shape[-2], mask.shape[-1])).movedim(1, -1).expand(-1, -1, -1, 3)
-     
-class CompareImages(Base_custom):
-    CATEGORY = "CG/images"
-    REQUIRED = { "image1": ("IMAGE",), "image2": ("IMAGE",), }
-    RETURN_TYPES = ("IMAGE","IMAGE")
-    RETURN_NAMES = ("i1_i2_diff","diff")
-
-    def func(self, image1:torch.Tensor, image2:torch.Tensor):
-        diff = torch.abs(image1-image2)
-        mean = torch.mean(diff,3)
-        result = torch.stack([mean for _ in range(3)],3)
-        combined = torch.cat((image1,image2,result),0)
-        return (combined, result, )
 
 class HardMask(Base_custom):
     CATEGORY = "CG/images"
@@ -36,4 +23,4 @@ class HardMask(Base_custom):
         return (m, mask_to_image(m),)
 
         
-CLAZZES = [CompareImages, HardMask]
+CLAZZES = [ HardMask]
