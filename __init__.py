909c5a4c636001f52df1148324adadcc62974c33
Chris <chris.goringe@gmail.com>
2023-09-26 10:50:12 +1000

    tiny menu

--- __init__.py
+++ __init__.py
@@ -17,3 +17,12 @@ for module in [os.path.splitext(f)[0] for f in os.listdir(module_src_directory) 
             NODE_DISPLAY_NAME_MAPPINGS[name] = pretty(name)
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+import shutil
+import folder_paths
+
+module_js_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), "js")
+application_root_directory = os.path.dirname(folder_paths.__file__)
+application_web_extensions_directory = os.path.join(application_root_directory, "web", "extensions", "A_UNIQUE_NAME")
+
+shutil.copytree(module_js_directory, application_web_extensions_directory, dirs_exist_ok=True)
